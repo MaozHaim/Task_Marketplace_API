0b67fb292193545f2ba4ad3b48348b833a8ec093
Maoz1005 <maozhaim@mail.tau.ac.il>
2026-02-08 21:59:00 +0200

    Fixed views logic

--- marketplace/views.py
+++ marketplace/views.py
@@ -1,10 +1,14 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 from .models import Job, Application
 from .serializers import JobSerializer, ApplicationSerializer
 
 class JobViewSet(viewsets.ModelViewSet):
     queryset = Job.objects.all().order_by('-created_at')
     serializer_class = JobSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    def perform_create(self, serializer):
+        # Fill 'owner' field automatically
+        serializer.save(owner=self.request.user)
 
 class ApplicationViewSet(viewsets.ModelViewSet):
     queryset = Application.objects.all().order_by('-submitted_at')
