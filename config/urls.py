d820d8b9156f48ee3a7de686f3c98f3ca5d1651a
Maoz1005 <maozhaim@mail.tau.ac.il>
2026-02-09 20:43:31 +0200

    Small fixes

--- config/urls.py
+++ config/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('marketplace/', include('marketplace.urls')),
+    path('api-auth/', include('rest_framework.urls')),
 ]
