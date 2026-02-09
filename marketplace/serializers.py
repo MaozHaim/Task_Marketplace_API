051bf8099f8ac1016edd8633690d03a9bbfbec2d
Maoz1005 <maozhaim@mail.tau.ac.il>
2026-02-09 19:09:57 +0200

    serializers.py - include both username and id in the JSON

--- marketplace/serializers.py
+++ marketplace/serializers.py
@@ -3,17 +3,24 @@ from .models import Job, Application
 
 
 class JobSerializer(serializers.ModelSerializer):
+    owner_name = serializers.ReadOnlyField(source='owner.username')
+    owner_id = serializers.ReadOnlyField(source='owner.id')
+
     class Meta:
         model = Job
-        fields = '__all__'
-        # The owner is the user that posted the job
-        # The user can't mark that he is hired and can't change the creation time of the job
-        read_only_fields = ['owner', 'created_at', 'status']
+        fields = ['id', 'owner_name', 'owner_id', 'title', 'description', 'created_at', 'status', 'applications']
+        # The user can't change the status, can't change the creation time of the job,
+        # and can't change his name or id
+        read_only_fields = ['status', 'created_at', 'owner_name', 'owner_id']
 
 
 class ApplicationSerializer(serializers.ModelSerializer):
+    freelancer_name = serializers.ReadOnlyField(source='freelancer.username')
+    freelancer_id = serializers.ReadOnlyField(source='freelancer.id')
+
     class Meta:
         model = Application
-        fields = '__all__'
-        # The user can't mark that he is hired and can't change the submission time of his application
-        read_only_fields = ['is_hired', 'submitted_at']
+        fields = ['id', 'job', 'freelancer_name', 'freelancer_id', 'bid_price', 'is_hired', 'submitted_at']
+        # The user can't mark that he is hired, can't change the submission time of his application,
+        # and can't change his name or id
+        read_only_fields = ['is_hired', 'submitted_at', 'freelancer_name', 'freelancer_id']
