d29bb048a74113507d965106125ab936588cfa64
Maoz1005 <maozhaim@mail.tau.ac.il>
2026-02-09 20:35:09 +0200

    Small fixes

--- marketplace/serializers.py
+++ marketplace/serializers.py
@@ -23,4 +23,4 @@ class JobSerializer(serializers.ModelSerializer):
         fields = ['id', 'owner_name', 'owner_id', 'title', 'description', 'created_at', 'status', 'applications']
         # The user can't change the status, can't change the creation time of the job,
         # and can't change his name or id
-        read_only_fields = ['status', 'created_at', 'owner_name', 'owner_id']
+        read_only_fields = ['status', 'created_at', 'owner_name', 'owner_id', 'applications']
